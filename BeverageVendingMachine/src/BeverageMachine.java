7c86a9c22cabcf13949b21bfb15308b234351a70
TyWilliams <info@byty.me>
2015-12-09 14:06:52 -0500

    first commit

---
+++ BeverageVendingMachine/src/BeverageMachine.java
@@ -0,0 +1,11 @@
+
+public class BeverageMachine {
+	private Product[]products;
+	
+	public BeverageMachine(){
+		int number of products = 0;
+		products = number of products ["number of products"];
+		
+	}
+
+}
