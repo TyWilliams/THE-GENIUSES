8712dd00d88fdd37f27fa38fe65082f14ea621fe
carlosringer <carlosringer@gmail.com>
2015-12-10 11:11:27 -0500

    @carlosringer added comments to enumeration classes

--- BeverageVendingMachine/src/products/Currency.java
+++ BeverageVendingMachine/src/products/Currency.java
@@ -1,5 +1,6 @@
 package products;
 
+// Creating enumerations to be called in the main method
 public enum Currency {
 nickel, dime, quarter, dollar
 }
--- BeverageVendingMachine/src/products/SelectionMenu.java
+++ BeverageVendingMachine/src/products/SelectionMenu.java
@@ -1,7 +1,8 @@
 package products;
 
+//Creating enumerations class specifing the product selections implemented in the main method
 public enum SelectionMenu {
 
-cocaCola, pepsi, mountainDew, grapeSoda, orangeSoda, vernors, water, cancel 	
+	cocaCola, pepsi, mountainDew, grapeSoda, orangeSoda, vernors, water, cancel
 
 }
