ad2b48f5bacc7143452a8d93887a115c125f22f1
carlosringer <carlosringer@gmail.com>
2015-12-10 12:09:43 -0500

    @carlosringer added comments to product.java class

--- BeverageVendingMachine/src/products/Currency.java
+++ BeverageVendingMachine/src/products/Currency.java
@@ -1,6 +1,6 @@
 package products;
 
-// Creating enumerations to be called in the main method
+// Creating enumerations class specifing the currency implemented in the main method
 public enum Currency {
 nickel, dime, quarter, dollar
 }
